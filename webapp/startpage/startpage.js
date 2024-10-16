92af2879c40df0fc2b5eef0ab231bb0eaecd9a91
penfoldslaw <penfoldslaws@gmail.com>
2024-10-16 00:06:03 -0400

    adding docker

--- webapp/startpage/startpage.js
+++ webapp/startpage/startpage.js
@@ -1,3 +1,4 @@
+
 const generate_button = document.getElementById("generate_button");
 
 generate_button.onclick = function generatebalance () {
@@ -6,5 +7,14 @@ const random_number = Math.floor(Math.random() * 20000) +10;
 
 document.getElementById('javascript').textContent = random_number;
 
+
+document.getElementById('account_balance_display').textContent = random_number;
+
+
+
 }
+
+generate_button.addEventListener('click', function(){
+	document.getElementById('generate_comment').style.display = 'none';});
+
 
